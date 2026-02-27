350de483cbc02acef346ec8626e0f12c1c8f8e9e
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-02-27 14:14:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // I bumped the version to v19 to force your new changes to load
-const CACHE_NAME = 'pranima-v20-user-updates'; 
+const CACHE_NAME = 'pranima-v21-user-updates'; 
 
 const ASSETS = [
   './',
